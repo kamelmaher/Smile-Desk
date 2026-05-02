cfd68dc7634c9fa2687049b8a5ea148db6869500
kamelmaher <kamelmahersh@gmail.com>
2026-05-02 13:12:45 +0300

    front-end url

--- back-end/index.js
+++ back-end/index.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser")
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.WEBSITE_URL,
     credentials: true
 }));
 app.use(cookieParser())
